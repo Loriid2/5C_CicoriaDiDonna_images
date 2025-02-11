d865c60c9cf4b809e051b8c5fcc892915c30ea25
unknown <dilorenzo@itis-molinari.eu>
2025-02-11 13:20:14 +0100

    aggiunte funzioni send, delete, get llato client

--- public/index.js
+++ public/index.js
@@ -16,3 +16,35 @@ loginForm.build(loginFormSetup, "loginModal");
 //console.log(loginForm) ;
 loginForm.render(null);
 loginForm.onsubmit(() => {}) ;
+const send=(img)=>{
+    fetch("/todo/add", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(img),
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          console.log("Risultato della POST:", result);
+        });
+}
+const getImages = () => {
+    fetch("/image")
+    .then((response) => response.json())
+    .then((data) => {
+       // logica della get lato client
+    }); 
+}
+const remove = (id)=>{
+    fetch(`"/image/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          //logica della delete lato client
+        });
+}
