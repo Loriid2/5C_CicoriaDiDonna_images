80c6254157ab687c4f2c308a18967984964fafc7
DavideCicoria <cicoriadavide@itis-molinari.eu>
2025-02-16 14:23:19 +0100

    modifiche grafica e fix di alcuni bug

--- public/index.js
+++ public/index.js
@@ -4,14 +4,13 @@ import { createNavigator } from "./src/navigatorComponent/navigator.js";
 const carouselInner = document.querySelector('#carouselInner');
 const adminButton = document.querySelector('#adminButton');
 const navigator = createNavigator();
-location.href = "#home";
 const loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
 const inputFile = document.querySelector('#file');
-    const button = document.querySelector("#buttonUpload");
-    const link = document.querySelector("#link");
-    const fileListContainer = document.querySelector("#fileList");
+const button = document.querySelector("#buttonUpload");
+const link = document.querySelector("#link");
+const fileListContainer = document.querySelector("#fileList");
 
-const loginComponent = generateLoginComponent(document.getElementById('loginModalBody')) ;
+const loginComponent = generateLoginComponent(document.getElementById('loginModalBody'));
 let images = [];
 
 const loginFormSetup = {
@@ -19,27 +18,27 @@ const loginFormSetup = {
 	"password": ["password", "input margin"],
 };
 
-const interval = window.setInterval(function(){
+const interval = window.setInterval(function () {
 	if (loginComponent.isLogged()) {
-		document.querySelector(".loginButton").classList.add("hide") ;
-		document.querySelector("#adminPage").classList.remove("hide") ;
+		document.querySelector(".loginButton").classList.add("hide");
+		document.querySelector("#adminPage").classList.remove("hide");
 		loginComponent.render();
 		clearInterval(interval);
 	}
 }, 2000);
 
 fetch("/conf.json")
-.then(r => r.json())
-.then(data => {
-	loginComponent.build(data.cache.token, 'private');
-	loginComponent.render();
-	if (loginComponent.isLogged()) {
-		document.querySelector(".loginButton").classList.add("hide") ;
-		document.querySelector("#adminPage").classList.remove("hide") ;
+	.then(r => r.json())
+	.then(data => {
+		loginComponent.build(data.cache.token, 'private');
 		loginComponent.render();
-		clearInterval(interval);
-	}
-});
+		if (loginComponent.isLogged()) {
+			document.querySelector(".loginButton").classList.add("hide");
+			document.querySelector("#adminPage").classList.remove("hide");
+			loginComponent.render();
+			clearInterval(interval);
+		}
+	});
 
 
 const send = (img) => {
@@ -72,11 +71,11 @@ const remove = async (id) => {
 			"Content-Type": "application/json",
 		},
 	})
-	.then((response) => response.json())
-	.then((data) => {
-		console.log(data) ;
-		render() ;
-	});
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data);
+			render();
+		});
 }
 
 const render = () => {
@@ -100,14 +99,14 @@ getImages();
 render();
 
 (async () => {
-    
-    const loadFileList =  () => {
-            fetch("/image")
-                .then(res => {return res.json();})
-                .then(files => {
-					console.log(files.images);
-			
-					let htmlContent = `
+
+	const loadFileList = () => {
+		fetch("/image")
+			.then(res => { return res.json(); })
+			.then(files => {
+				console.log(files.images);
+
+				let htmlContent = `
 					<table class="table table-striped table-bordered table-hover margin">
 						<thead class="table-dark">
 							<tr>
@@ -117,8 +116,8 @@ render();
 							</tr>
 						</thead>
 						<tbody>`;
-					files.images.forEach((fileUrl, index) => {
-						htmlContent += `
+				files.images.forEach((fileUrl, index) => {
+					htmlContent += `
 									<tr>
 										<td style="text-align: center; vertical-align: middle;">
 											<img src="${fileUrl.url}" width="30px" height="30px" class="d-block">
@@ -130,48 +129,53 @@ render();
 											<button id="${fileUrl.id}" class="eliminaButton btn btn-danger">Elimina</button>
 										</td>
 									</tr>`;
-					});
-					htmlContent += '</tbody></table>' ;
-					
-					fileListContainer.innerHTML = htmlContent;
-					console.log(fileListContainer.innerHTML);
+				});
+				htmlContent += '</tbody></table>';
+
+				fileListContainer.innerHTML = htmlContent;
+				console.log(fileListContainer.innerHTML);
 
 
-					document.querySelectorAll('.eliminaButton').forEach((e) => {
-						console.log(e);
-						e.onclick = async () => {
-							await remove(e.id) ;
-							await render() ;
-							await loadFileList() ;
-						}
-					})
-                });
-				
-    };
+				document.querySelectorAll('.eliminaButton').forEach((e) => {
+					console.log(e);
+					e.onclick = async () => {
+						await remove(e.id);
+						await render();
+						await loadFileList();
+					}
+				})
+			});
 
-	const handleSubmit  = async () => {
-		const formData = new FormData();
-		formData.append("file", inputFile.files[0]);
-	  
-		try {
-		  const response = await fetch(`/image/add`, {
-			method: "POST",
-			body: formData,
-		  });
-		  const result = await response.json();
-		  console.log("Risultato della POST:", result);
-		  link.href = result.url;
-		  loadFileList();
-		} catch (e) {
-		  console.log(e);
+	};
+
+	const handleSubmit = async () => {
+		const fileInput = document.getElementById('file') ;
+		if (fileInput.value) {
+			const formData = new FormData();
+			formData.append("file", inputFile.files[0]);
+	
+			try {
+				const response = await fetch(`/image/add`, {
+					method: "POST",
+					body: formData,
+				});
+				const result = await response.json();
+				console.log("Risultato della POST:", result);
+				link.href = result.url;
+				loadFileList();
+			} catch (e) {
+				console.log(e);
+			}
+		} else {
+			console.log("input vuoto") ;
 		}
-	  };
-	  
+	};
+
 	button.onclick = handleSubmit;
 
-    
 
-    loadFileList();
+
+	loadFileList();
 	getImages();
 	render();
 })();
